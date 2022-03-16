2567cc1eaa8f3a215251f89224b1e20359249e2b
PawelMolek <pawel.molek@gmail.com>
2022-03-16 21:36:48 +0100

    Show page updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
   validates :content, presence: true, length: { minimum: 30 }
 
-
-
   belongs_to :user
 
   # because of dependent: :destroy we can delete post with comments without sqlite3 error
