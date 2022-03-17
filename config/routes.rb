b28b098416574706b8f31369339a23d92d65e471
PawelMolek <pawel.molek@gmail.com>
2022-03-17 14:38:30 +0100

    home controller created including contact and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  # Defines the root path route ("/")
+
   root "posts#index"
+  get "home/about"
+  get "home/contact"
 end
